4fdeea0f3f279d5d08c20f04b1dce9795351da37
PABLO CRUZ MENDEZ <pablo.cruz@utmac0006.local>
2018-10-17 13:20:19 +0200

    Initial Commit

---
+++ Prueva1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueva1
+//
+//  Created by PABLO CRUZ MENDEZ on 17/10/18.
+//  Copyright © 2018 PABLO CRUZ MENDEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
